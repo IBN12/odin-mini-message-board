02c84f61a626194af1ef0cec69e2614586203cd8
IBN12 <49168983+IBN12@users.noreply.github.com>
2025-09-09 02:26:49 -0700

    Refactored the pool.js file and userPostController.js file.

--- controllers/usersPostController.js
+++ controllers/usersPostController.js
@@ -3,7 +3,7 @@ import db from "../models/queries.js";
 // userPostControllerGet(): Will get all the data in our table to display on the index page.
 const userPostControllerGet = async (req, res) => {
     const messages = await db.getAllUserPosts();
-    console.log(messages); // Testing 
+    // console.log(messages); // Testing 
     
     res.status(200).render("index", {
         title: "MINI MESSAGEBOARD", 
--- models/pool.js
+++ models/pool.js
@@ -11,5 +11,7 @@ import 'dotenv/config';
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized:  false } : false, 
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
