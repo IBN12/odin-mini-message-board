e19703db9206c59818433384f41e2a0e89eab624
IBN12 <49168983+IBN12@users.noreply.github.com>
2025-09-09 01:13:14 -0700

    The database has been connected to Railway.

--- models/pool.js
+++ models/pool.js
@@ -1,10 +1,15 @@
 import { Pool } from "pg";
 import 'dotenv/config';
 
+// export const pool = new Pool({
+//     host: process.env.host,
+//     user: process.env.user,
+//     database: process.env.database,
+//     password: process.env.password,
+//     port: process.env.port,
+// }); 
+
 export const pool = new Pool({
-    host: process.env.host,
-    user: process.env.user,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.port,
-}); 
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized:  false } : false, 
+});
